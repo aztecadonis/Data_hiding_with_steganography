9a7ef3bab25562aa982d6c21a182e10922f42a28
aztecadonis <wellsayush@gmail.com>
2025-02-26 11:15:44 +0530

    Add files via upload

---
+++ decryption.py
@@ -0,0 +1,13 @@
+import cv2
+import os
+import string
+pas = input("Enter passcode for Decryption")
+if password == pas:
+    for i in range(len(msg)):
+        message = message + c[img[n, m, z]]
+        n = n + 1
+        m = m + 1
+        z = (z + 1) % 3
+    print("Decryption message:", message)
+else:
+    print("YOU ARE NOT auth")
